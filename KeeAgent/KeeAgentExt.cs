c363bcbcc370f7f5c038fbfdef10a09c5a2b45ad
dlech <gismho@gmail.com>
2012-05-23 23:40:11 -0500

    remove debug requirement from test and bump assy version

--- KeeAgent/KeeAgentExt.cs
+++ KeeAgent/KeeAgentExt.cs
@@ -20,225 +20,222 @@ using KeePass.App;
 
 namespace KeeAgent
 {
-	public sealed class KeeAgentExt : Plugin
-	{
-		internal IPluginHost pluginHost;
-		private WinPageant pageant;
-		private ToolStripMenuItem keeAgentMenuItem;
-		private UIHelper uiHelper;
+    public sealed class KeeAgentExt : Plugin
+    {
+        internal IPluginHost pluginHost;
+        private WinPageant pageant;
+        private ToolStripMenuItem keeAgentMenuItem;
+        private UIHelper uiHelper;
 
-		public override bool Initialize(IPluginHost host)
-		{
-			bool result;
+        public override bool Initialize(IPluginHost host)
+        {
+            bool result;
 
-			this.pluginHost = host;
-			this.uiHelper = new UIHelper(this.pluginHost);
+            this.pluginHost = host;
+            this.uiHelper = new UIHelper(this.pluginHost);
 
-			try {
-				// TODO check OS - currently only works on Windows
-				this.pageant = new WinPageant(GetPpkKeyList, GetSSH2Key);
-				result = true;
-			} catch (Exception) {
-				ShowPageantRunningErrorMessage();
-				result = false;
-			}
+            try {
+                // TODO check OS - currently only works on Windows
+                this.pageant = new WinPageant(GetPpkKeyList, GetSSH2Key);
+                result = true;
+            } catch (Exception) {
+                ShowPageantRunningErrorMessage();
+                result = false;
+            }
 
-			InvokeMainWindow(new MethodInvoker(AddMenuItems));
+            InvokeMainWindow(new MethodInvoker(AddMenuItems));
 
-			return result;
-		}
+            return result;
+        }
 
-		public override void Terminate()
-		{			
-			if (this.pageant != null) {
-				this.pageant.Dispose();
-			}
-			InvokeMainWindow(new MethodInvoker(RemoveMenuItems));
-		}
+        public override void Terminate()
+        {
+            if (this.pageant != null) {
+                this.pageant.Dispose();
+            }
+            InvokeMainWindow(new MethodInvoker(RemoveMenuItems));
+        }
 
-		public override Image SmallIcon
-		{
-			get { return Resources.KeeAgentIcon; }
-		}
-	
-		/// <summary>
-		/// Returns url for automatic updating of plugin
-		/// </summary>
-		public override string UpdateUrl
-		{
-			get { return "http://updates.lechnology.com/KeePassPlugins"; }
-		}
+        public override Image SmallIcon
+        {
+            get { return Resources.KeeAgentIcon; }
+        }
+
+        /// <summary>
+        /// Returns url for automatic updating of plugin
+        /// </summary>
+        public override string UpdateUrl
+        {
+            get { return "http://updates.lechnology.com/KeePassPlugins"; }
+        }
 
 
-		private void ShowPageantRunningErrorMessage()
-		{
-			MessageService.ShowWarning(new object[] { Translatable.ErrPageantRunning });
-		}
+        private void ShowPageantRunningErrorMessage()
+        {
+            MessageService.ShowWarning(new object[] { Translatable.ErrPageantRunning });
+        }
 
-		private void AddMenuItems()
-		{
-			/* get Tools menu */
-			ToolStripMenuItem toolsMenu = this.pluginHost.MainWindow.ToolsMenu;
+        private void AddMenuItems()
+        {
+            /* get Tools menu */
+            ToolStripMenuItem toolsMenu = this.pluginHost.MainWindow.ToolsMenu;
 
-			/* create parent menu item */
-			keeAgentMenuItem = new ToolStripMenuItem();
-			keeAgentMenuItem.Text = Assembly.GetExecutingAssembly().GetName().Name;
+            /* create parent menu item */
+            keeAgentMenuItem = new ToolStripMenuItem();
+            keeAgentMenuItem.Text = Assembly.GetExecutingAssembly().GetName().Name;
 
-			if (pageant != null) {
-				/* create children menu items */
-				ToolStripMenuItem keeAgentListPuttyKeysMenuItem = new ToolStripMenuItem();
-				keeAgentListPuttyKeysMenuItem.Text = Translatable.ShowPuttyKeysMenuItem;
-				keeAgentListPuttyKeysMenuItem.ToolTipText = Translatable.ShowPuttyKeysMenuItemToolTip;
-				keeAgentListPuttyKeysMenuItem.Click += new EventHandler(keeAgentListPuttyKeysMenuItem_Click);
+            if (pageant != null) {
+                /* create children menu items */
+                ToolStripMenuItem keeAgentListPuttyKeysMenuItem = new ToolStripMenuItem();
+                keeAgentListPuttyKeysMenuItem.Text = Translatable.ShowPuttyKeysMenuItem;
+                keeAgentListPuttyKeysMenuItem.ToolTipText = Translatable.ShowPuttyKeysMenuItemToolTip;
+                keeAgentListPuttyKeysMenuItem.Click += new EventHandler(keeAgentListPuttyKeysMenuItem_Click);
 
-				/* add children to parent */
-				keeAgentMenuItem.DropDownItems.Add(keeAgentListPuttyKeysMenuItem);
-			} else {
-				keeAgentMenuItem.Enabled = false;
-			}
+                /* add children to parent */
+                keeAgentMenuItem.DropDownItems.Add(keeAgentListPuttyKeysMenuItem);
+            } else {
+                keeAgentMenuItem.Enabled = false;
+            }
 
-			/* add new items to tools menu */
-			toolsMenu.DropDownItems.Add(keeAgentMenuItem);
+            /* add new items to tools menu */
+            toolsMenu.DropDownItems.Add(keeAgentMenuItem);
 
-		}
+        }
 
-		private void RemoveMenuItems()
-		{
-			if (this.pluginHost != null && this.pluginHost.MainWindow != null && 
-				this.keeAgentMenuItem != null) {
+        private void RemoveMenuItems()
+        {
+            if (this.pluginHost != null && this.pluginHost.MainWindow != null &&
+                this.keeAgentMenuItem != null) {
 
-				/* get Tools menu */
-				ToolStripMenuItem toolsMenu = this.pluginHost.MainWindow.ToolsMenu;
-				/* remove items from tools menu */
-				toolsMenu.DropDownItems.Remove(keeAgentMenuItem);
-			}
-		}
+                /* get Tools menu */
+                ToolStripMenuItem toolsMenu = this.pluginHost.MainWindow.ToolsMenu;
+                /* remove items from tools menu */
+                toolsMenu.DropDownItems.Remove(keeAgentMenuItem);
+            }
+        }
 
-		private void keeAgentListPuttyKeysMenuItem_Click(object source, EventArgs e)
-		{
-			KeyListDialog dialog = new KeyListDialog(this);
-			DialogResult result = dialog.ShowDialog(pluginHost.MainWindow);
-			dialog.Dispose();
-		}
+        private void keeAgentListPuttyKeysMenuItem_Click(object source, EventArgs e)
+        {
+            KeyListDialog dialog = new KeyListDialog(this);
+            DialogResult result = dialog.ShowDialog(pluginHost.MainWindow);
+            dialog.Dispose();
+        }
 
-		private void InvokeMainWindow(MethodInvoker methodInvoker)
-		{
-			Form mainWindow = this.pluginHost.MainWindow;
+        private void InvokeMainWindow(MethodInvoker methodInvoker)
+        {
+            Form mainWindow = this.pluginHost.MainWindow;
 
-			if (mainWindow.InvokeRequired) {
-				mainWindow.Invoke(methodInvoker);
-			} else {
-				methodInvoker.Invoke();
-			}
-		}
+            if (mainWindow.InvokeRequired) {
+                mainWindow.Invoke(methodInvoker);
+            } else {
+                methodInvoker.Invoke();
+            }
+        }
 
-		internal IEnumerable<PpkKey> GetPpkKeyList()
-		{
-			return (IEnumerable<PpkKey>)GetKeeAgentKeyList();
-		}
+        internal IEnumerable<PpkKey> GetPpkKeyList()
+        {
+            return (IEnumerable<PpkKey>)GetKeeAgentKeyList();
+        }
 
-		internal IEnumerable<KeeAgentKey> GetKeeAgentKeyList()
-		{
-			pluginHost.MainWindow.NotifyUserActivity();
+        internal IEnumerable<KeeAgentKey> GetKeeAgentKeyList()
+        {
+            pluginHost.MainWindow.NotifyUserActivity();
 
-			List<KeeAgentKey> keyList = new List<KeeAgentKey>();
+            List<KeeAgentKey> keyList = new List<KeeAgentKey>();
 
-			if (this.pluginHost != null && this.pluginHost.Database != null) {
+            if (this.pluginHost != null && this.pluginHost.Database != null) {
 
-				foreach (PwEntry entry in this.pluginHost.Database.RootGroup.GetEntries(true)) {
-					foreach (KeyValuePair<string, ProtectedBinary> bin in entry.Binaries) {
+                foreach (PwEntry entry in this.pluginHost.Database.RootGroup.GetEntries(true)) {
+                    foreach (KeyValuePair<string, ProtectedBinary> bin in entry.Binaries) {
 
-						/* handle PuTTY Private Key files */
+                        /* handle PuTTY Private Key files */
 
-						if (bin.Key.EndsWith(".ppk")) {
-							try { 
-								SecureString passphrase = null;
-								CryptoRandomStream crsRandomSource;
-								ProtectedString passphraseFromKeepass = entry.Strings.Get(PwDefs.PasswordField);
-								if (passphraseFromKeepass != null) {
-									/* use random bytes to protect passphrase in memory */
-									byte[] randomBytes = CryptoRandom.Instance.GetRandomBytes(256);
-									crsRandomSource = new CryptoRandomStream(CrsAlgorithm.Salsa20, randomBytes);
-									byte[] passphraseBytes = passphraseFromKeepass.ReadXorredString(crsRandomSource);
-									crsRandomSource = new CryptoRandomStream(CrsAlgorithm.Salsa20, randomBytes);
-									randomBytes = crsRandomSource.GetRandomBytes((uint)passphraseBytes.Length);
-									passphrase = new SecureString();
-									/* convert passphrase from KeePass protected format to .NET protected format */
-									for (int i = 0; i < passphraseBytes.Length; i++) {
-										passphrase.AppendChar((char)(passphraseBytes[i] ^ randomBytes[i]));
-									}
-									Array.Clear(passphraseBytes, 0, passphraseBytes.Length);
-								}
-
-								PpkFile.GetPassphraseCallback getPassphrase = delegate()
-								{
-									return passphrase;
-								};
-
-								PpkFile.WarnOldFileFormatCallback warnUser = delegate()
-								{
-									// we will warn user a different way... won't we???
-								};
-
-								PpkKey ppkKey  = PpkFile.ParseData(bin.Value.ReadData(), getPassphrase, warnUser);
-								KeeAgentKey key = new KeeAgentKey(ppkKey, entry.Uuid, bin.Key);
-								keyList.Add(key);
-							} catch (Exception ex) {
-                                if (Program.CommandLineArgs[AppDefs.CommandLineOptions.Debug] != null)
-                                {
-                                    MessageService.ShowWarning("Error while loading key from ", entry.Strings.Get(PwDefs.TitleField).ReadString(), ex.ToString());
+                        if (bin.Key.EndsWith(".ppk")) {
+                            try {
+                                SecureString passphrase = null;
+                                CryptoRandomStream crsRandomSource;
+                                ProtectedString passphraseFromKeepass = entry.Strings.Get(PwDefs.PasswordField);
+                                if (passphraseFromKeepass != null) {
+                                    /* use random bytes to protect passphrase in memory */
+                                    byte[] randomBytes = CryptoRandom.Instance.GetRandomBytes(256);
+                                    crsRandomSource = new CryptoRandomStream(CrsAlgorithm.Salsa20, randomBytes);
+                                    byte[] passphraseBytes = passphraseFromKeepass.ReadXorredString(crsRandomSource);
+                                    crsRandomSource = new CryptoRandomStream(CrsAlgorithm.Salsa20, randomBytes);
+                                    randomBytes = crsRandomSource.GetRandomBytes((uint)passphraseBytes.Length);
+                                    passphrase = new SecureString();
+                                    /* convert passphrase from KeePass protected format to .NET protected format */
+                                    for (int i = 0; i < passphraseBytes.Length; i++) {
+                                        passphrase.AppendChar((char)(passphraseBytes[i] ^ randomBytes[i]));
+                                    }
+                                    Array.Clear(passphraseBytes, 0, passphraseBytes.Length);
                                 }
-								Debug.Fail(ex.ToString());
-								// ignore errors - for now
-							}
-						} // end .ppk file
-					}
-				}
-			}
-			return keyList;
-		}
 
-		internal PpkKey GetSSH2Key(byte[] fingerprint)
-		{
-			pluginHost.MainWindow.NotifyUserActivity();
+                                PpkFile.GetPassphraseCallback getPassphrase = delegate()
+                                {
+                                    return passphrase;
+                                };
 
-			this.uiHelper.ShowNotification(Translatable.NotifyKeyFetched);
-			
-			/* TODO it would probably be better if we cached the fingerprints and mapped them
-			 * to the PwEntry Uuid rather than regenerating the full list to get
-			 * a single key as we are doing here. 
-			 */
+                                PpkFile.WarnOldFileFormatCallback warnUser = delegate()
+                                {
+                                    // we will warn user a different way... won't we???
+                                };
 
-			IEnumerable<PpkKey> ppkKeyList = GetPpkKeyList();
-			PpkKey result = null;
-			foreach (PpkKey ppkKey in ppkKeyList) {
-				if (result == null) {
-					try {
-						byte[] testFingerprint = ppkKey.GetFingerprint();
-						if (testFingerprint.Length == fingerprint.Length) {
-							bool match = true;
-							for (int i = 0; i < testFingerprint.Length; i++) {
-								if (testFingerprint[i] != fingerprint[i]) {
-									match = false;
-									break;
-								}
-							}
-							if (match) {
-								result = ppkKey;
-							}
-						}
-					} catch (Exception ex) {
-						Debug.Fail(ex.ToString());
-					}
-				}
-				// dispose all keys except for the one match
-				if (ppkKey != result) {
-					ppkKey.Dispose();
-				}
-			}
+                                PpkKey ppkKey = PpkFile.ParseData(bin.Value.ReadData(), getPassphrase, warnUser);
+                                KeeAgentKey key = new KeeAgentKey(ppkKey, entry.Uuid, bin.Key);
+                                keyList.Add(key);
+                            } catch (Exception ex) {
+                                MessageService.ShowWarning("Error while loading key from ", entry.Strings.Get(PwDefs.TitleField).ReadString(), ex.ToString());
+                                Debug.Fail(ex.ToString());
+                                // ignore errors - for now
+                            }
+                        } // end .ppk file
+                    }
+                }
+            }
+            return keyList;
+        }
 
-			return result;
-		}
+        internal PpkKey GetSSH2Key(byte[] fingerprint)
+        {
+            pluginHost.MainWindow.NotifyUserActivity();
 
-	} // class
+            this.uiHelper.ShowNotification(Translatable.NotifyKeyFetched);
+
+            /* TODO it would probably be better if we cached the fingerprints and mapped them
+             * to the PwEntry Uuid rather than regenerating the full list to get
+             * a single key as we are doing here. 
+             */
+
+            IEnumerable<PpkKey> ppkKeyList = GetPpkKeyList();
+            PpkKey result = null;
+            foreach (PpkKey ppkKey in ppkKeyList) {
+                if (result == null) {
+                    try {
+                        byte[] testFingerprint = ppkKey.GetFingerprint();
+                        if (testFingerprint.Length == fingerprint.Length) {
+                            bool match = true;
+                            for (int i = 0; i < testFingerprint.Length; i++) {
+                                if (testFingerprint[i] != fingerprint[i]) {
+                                    match = false;
+                                    break;
+                                }
+                            }
+                            if (match) {
+                                result = ppkKey;
+                            }
+                        }
+                    } catch (Exception ex) {
+                        Debug.Fail(ex.ToString());
+                    }
+                }
+                // dispose all keys except for the one match
+                if (ppkKey != result) {
+                    ppkKey.Dispose();
+                }
+            }
+
+            return result;
+        }
+
+    } // class
 } // namespace
