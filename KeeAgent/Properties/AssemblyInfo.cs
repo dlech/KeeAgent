617c2cead0d592026e3c544f2c9da0f0cbac6782
David Lechner <david@lechnology.com>
2012-06-16 13:41:45 -0500

    bump version for release

--- KeeAgent/Properties/AssemblyInfo.cs
+++ KeeAgent/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.1.4.3")]
-[assembly: AssemblyFileVersion("0.1.4.3")]
+[assembly: AssemblyVersion("0.1.5.0")]
+[assembly: AssemblyFileVersion("0.1.5.0")]
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("KeeAgentTestProject")]
