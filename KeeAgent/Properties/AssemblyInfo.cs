11213205410e816931b3897983aafd1b701df79b
David Lechner <david@lechnology.com>
2013-08-07 12:50:47 -0500

    Version Bump

--- KeeAgent/Properties/AssemblyInfo.cs
+++ KeeAgent/Properties/AssemblyInfo.cs
@@ -50,5 +50,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.3.11.0")]
-[assembly: AssemblyFileVersion("0.3.11.0")]
+[assembly: AssemblyVersion ("0.4.0.0")]
+[assembly: AssemblyFileVersion ("0.4.0.0")]
