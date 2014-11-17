1bc0bd7765d21a19cd6300094f89a17e7d7f6abc
David Lechner <david@lechnology.com>
2014-11-17 15:55:59 -0600

    remove TODO since v2.17 is already over 10 versions (3 years) old.

--- KeeAgent/KeeAgentExt.cs
+++ KeeAgent/KeeAgentExt.cs
@@ -203,8 +203,6 @@ namespace KeeAgent
     /// </summary> 
     public override string UpdateUrl
     {
-      // TODO - find a way to implement this using something like Castle
-      // DynamicProxy or LinFu so that we can compile against KeePass <= 2.17
       get { return "http://updates.lechnology.com/KeePassPlugins"; }
     }
 
