25fefac113e43dba52608a4c372f81bb3fdd75d8
David Lechner <david@lechnology.com>
2014-11-18 22:15:19 -0600

    release

--- KeeAgent/Properties/AssemblyInfo.cs
+++ KeeAgent/Properties/AssemblyInfo.cs
@@ -50,5 +50,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion ("0.5.3.0")]
-[assembly: AssemblyFileVersion ("0.5.3.0")]
+[assembly: AssemblyVersion ("0.5.4.0")]
+[assembly: AssemblyFileVersion ("0.5.4.0")]
