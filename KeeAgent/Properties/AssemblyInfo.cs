e4f1c7b5cc5c66801da541c06055c5017a7096a7
David Lechner <david@lechnology.com>
2016-03-21 21:00:53 -0500

    Version bump

--- KeeAgent/Properties/AssemblyInfo.cs
+++ KeeAgent/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("David Lechner")]
 [assembly: AssemblyProduct("KeePass Plugin")]
-[assembly: AssemblyCopyright("2012-2015")]
+[assembly: AssemblyCopyright("2012-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -50,5 +50,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.7.11.0")]
-[assembly: AssemblyFileVersion("0.7.11.0")]
+[assembly: AssemblyVersion("0.7.12.0")]
+[assembly: AssemblyFileVersion("0.7.12.0")]
