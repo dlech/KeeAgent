97532ce873df994aae771d64f757613fa9b6a587
David Lechner <david@lechnology.com>
2022-07-17 18:07:20 -0500

    v0.13.1

--- KeeAgent/Properties/AssemblyInfo.cs
+++ KeeAgent/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.13.0.*")]
-[assembly: AssemblyFileVersion("0.13.0.0")]
+[assembly: AssemblyVersion("0.13.1.0")]
+[assembly: AssemblyFileVersion("0.13.1.0")]
