d338f679043a52af9eac24a82fe05a9604e84e32
David Lechner <david@lechnology.com>
2024-05-04 11:01:33 -0500

    doc: update sphinx and theme versions

--- doc/conf.py
+++ doc/conf.py
@@ -75,7 +75,7 @@ version = '.'.join(release.split('.')[:2])
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en-us"
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
